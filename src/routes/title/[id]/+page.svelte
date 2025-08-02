c58a07118af267dddb7a6f903a4a8665a90fcd5f
biase-d <tashingachikwanha@gmail.com>
2025-08-03 00:23:58 +0200

    remove console.log

--- src/routes/title/[id]/+page.svelte
+++ src/routes/title/[id]/+page.svelte
@@ -117,8 +117,6 @@
 		const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
 		return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
 	}
-
-	console.log(url)
 </script>
 
 <svelte:head>
