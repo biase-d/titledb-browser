2517a0823df3d95cf405940f085cdc98b260b40d
biase-d <tashingachikwanha@gmail.com>
2026-02-14 19:25:24 +0200

    update

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-module.exports = {
-    apps: [
-        {
-            name: "titledb-browser",
-            script: "build/index.js",
-            env: {
-                NODE_ENV: "production",
-                PORT: 3000,
-                ORIGIN: "https://titledb.com",
-                BODY_SIZE_LIMIT: "Infinity"
-            }
-        }
-    ]
-};
+import './build/index.js';
