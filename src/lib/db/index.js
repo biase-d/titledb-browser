c30c33dc7c666aea4004beb440e4d9b415e5f9b7
biase-d <tashingachikwanha@gmail.com>
2026-02-15 23:38:04 +0200

    Unify database driver to postgres-js for local database compatibility

--- src/lib/db/index.js
+++ src/lib/db/index.js
@@ -1,20 +1,7 @@
-import { dev } from '$app/environment'
 import { POSTGRES_URL } from '$env/static/private'
 import * as schema from './schema'
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
-import { neon } from '@neondatabase/serverless'
+import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
 
-let dbInstance
-
-if (!dev) {
-	const client = neon(POSTGRES_URL)
-	dbInstance = drizzleNeon(client, { schema })
-} else {
-	const { default: postgres } = await import('postgres')
-	const { drizzle: drizzlePg } = await import('drizzle-orm/postgres-js')
-
-	const client = postgres(POSTGRES_URL)
-	dbInstance = drizzlePg(client, { schema })
-}
-
-export const db = dbInstance
+const client = postgres(POSTGRES_URL)
+export const db = drizzle(client, { schema })
